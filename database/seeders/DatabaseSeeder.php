355845fecb79c31b3c397d989d2635d94b423c21
Aki <103581174+Akii-Dev@users.noreply.github.com>
2024-12-17 12:51:52 +0100

    tags

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,9 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        User::factory(5)
-            ->hasProfile()
-            ->hasPosts(3)
-            ->create();
+        $this->call([
+            UserSeeder::class,
+            PostSeeder::class,
+        ]);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -13,12 +13,17 @@
 <body>
     <div class="flex justify-center  bg-white">
         <div class="mx-auto flex-row w-1/2 bg-white shadow-xl rounded font-medium p-8">
-            <h1 class="text-2xl font-bold mb-8">{{$post->title}}</h1>
+            <h1 class="text-2xl font-bold mb-8">{{ $post->title }}</h1>
+            <div class="flex">
+                @foreach ($post->tags as $tag)
+                <p class=' m-1 bg-[{{$tag->color}}]'>{{$tag->name}}</p>
+                @endforeach
+            </div>
             <div class="flex">
                 <div class="rounded-full w-10 h-10 bg-gray-200 mr-2 my-auto"></div>
                 <div class="flex-col">
-                    <p class="mb-4">{{$post->user->name}}</p>
-                    <p>{{$post->user->email}}</p>
+                    <p class="mb-4">{{ $post->user->name }}</p>
+                    <p>{{ $post->user->email }}</p>
                 </div>
             </div>
             <div class="mt-8">
